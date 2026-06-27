ad50e77df235f61f7448c5fb9dc09648d342ba9f
Nick-roy <nroy08232@gmail.com>
2026-06-27 12:04:35 +0600

    add

--- Components/shared/Navbar.jsx
+++ Components/shared/Navbar.jsx
@@ -15,9 +15,7 @@ export default function Navbar() {
     <>
       <nav className=" w-full z-50 pt-4">
         <div className="max-w-7xl mx-auto px-6">
-
           <div className="h-16 px-6 rounded-2xl border border-white/20 bg-white/10 backdrop-blur-lg shadow-lg flex items-center justify-between">
-
             {/* Logo */}
             <Link href="/" className="text-2xl font-bold">
               Doctors<span className="text-cyan-400">Point</span>
@@ -25,9 +23,7 @@ export default function Navbar() {
 
             {/* Desktop Menu */}
             <div className="hidden md:flex items-center gap-8">
-
               <ul className="flex items-center gap-8 px-6 py-3 rounded-full border border-white/10 bg-black/5 backdrop-blur-md font-medium">
-
                 <li>
                   <Link
                     href="/"
@@ -79,15 +75,22 @@ export default function Navbar() {
                     Contact
                   </Link>
                 </li>
-
               </ul>
 
-              <Link
-                href="/register"
-                className="bg-cyan-500 hover:bg-cyan-600 px-5 py-2 rounded-full text-white font-medium transition shadow-md"
-              >
-                Register
-              </Link>
+              <div className="flex  gap-2">
+                <Link
+                  href="/register"
+                  className="bg-cyan-500 hover:bg-cyan-600 px-5 py-2 rounded-full text-white font-medium transition shadow-md"
+                >
+                  Register
+                </Link>
+                <Link
+                  href="/register"
+                  className=" border-2 border-cyan-500 hover:bg-cyan-600 hover:text-white px-5 py-2 rounded-full  font-medium transition shadow-md"
+                >
+                  Get Start
+                </Link>
+              </div>
             </div>
 
             {/* Mobile Menu Button */}
@@ -107,7 +110,6 @@ export default function Navbar() {
           open ? "translate-y-0" : "-translate-y-full"
         }`}
       >
-
         <Link
           href="/"
           onClick={() => setOpen(false)}
@@ -121,7 +123,7 @@ export default function Navbar() {
         </Link>
 
         <Link href="/Consultant" onClick={() => setOpen(false)}>
-         Consultant
+          Consultant
         </Link>
 
         <Link href="/contact" onClick={() => setOpen(false)}>
@@ -134,6 +136,12 @@ export default function Navbar() {
           className="bg-cyan-500 hover:bg-cyan-600 px-6 py-3 rounded-full text-lg transition"
         >
           Register
+        </Link>
+        <Link
+          href="/register"
+          className=" border-2 border-cyan-500 hover:bg-cyan-600 hover:text-white px-5 py-2 rounded-full  font-medium transition shadow-md"
+        >
+          Get Start
         </Link>
       </div>
     </>
